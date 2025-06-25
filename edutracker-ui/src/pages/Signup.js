a8feca0309fbca49e0dbcbba40b2959473c33a3c
Vartika-505 <singhvartika110@gmail.com>
2025-06-25 17:35:07 +0530

    url modifications for deployment

--- edutracker-ui/src/pages/Signup.js
+++ edutracker-ui/src/pages/Signup.js
@@ -23,7 +23,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        `${process.env.REACT_APP_API_BASE_URL}/api/auth/signup`,
+        `${process.env.REACT_APP_API_URL}/api/auth/signup`,
         {
           username,
           email,
@@ -70,7 +70,7 @@ const handleGoogleSignIn = async (response) => {
 
   try {
     const googleSignInResponse = await axios.post(
-      `${process.env.REACT_APP_API_BASE_URL}/api/auth/googleSign`,
+      `${process.env.REACT_APP_API_URL}/api/auth/googleSign`,
       {
         googleId,
         username: googleDisplayName, 
@@ -97,7 +97,7 @@ const handleGoogleSignIn = async (response) => {
     } else {
       console.log("User does not exist, creating new user.");
       const newUserResponse = await axios.post(
-        `${process.env.REACT_APP_API_BASE_URL}/api/auth/signup`,
+        `${process.env.REACT_APP_API_URL}/api/auth/signup`,
         {
           email: decoded.email,
           username: googleDisplayName, 
